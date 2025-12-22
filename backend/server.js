ff3accfce8a771949c9bd1039bf6f203d704d89a
Vaibhav chauhan <75163092+Vaibhav040@users.noreply.github.com>
2025-12-22 13:13:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const port = PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// NOSONAR: intentionally allowing all origins
 app.use(cors());
 app.use(cookieParser());
 app.use(compression());
